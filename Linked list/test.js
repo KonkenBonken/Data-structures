401da34da68560779903036e543744984c0479e2
KonkenBonken <konrad05pettersson@gmail.com>
2022-05-11 18:47:39 +0200

    Improved logging for testing

--- Linked list/test.js
+++ Linked list/test.js
@@ -2,6 +2,11 @@ import { LinkedList } from './LinkedList.js';
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 const list = new LinkedList(array);
-console.log(...list);
-console.log('' + list);
-console.log(list.reverse());
+const log = {
+	traversed: [...list],
+	stringified: '' + list,
+	reversed: list.reverse(),
+	last: list.last,
+}
+
+console.log(log);
